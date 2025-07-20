c35e8b384da13c8b982e892cbb6bb068640c57e8
Min Kim <minkimcodes@gmail.com>
2025-07-20 16:43:57 -0400

    Refactor nav and hero

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <Header />
-      <main className="mx-auto flex max-w-6xl flex-col items-center gap-[32px] pt-24 row-start-2 sm:items-start">
+      <main className="pt-24 row-start-2 bg-white">
         <Hero />
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
