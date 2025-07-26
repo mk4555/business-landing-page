f650270c4465dd1427829d1c387032547412fa07
Min Kim <minkimcodes@gmail.com>
2025-07-26 19:30:12 -0400

    refactor

--- app/components/InfoSection.tsx
+++ app/components/InfoSection.tsx
@@ -33,15 +33,15 @@ const InfoSection: React.FC = () => {
             </p>
           </div>
           <div className="border p-6 rounded-lg shadow-md bg-white">
-            <NumberFlow
-              value={1000000}
-              prefix="$"
-              suffix="M in Costs Saved"
-              transformTiming={{ duration: 150, easing: "linear(...)" }}
-              animated={true}
-            />
-
-            <h3 className="text-xl font-bold text-sky-500 mb-2"></h3>
+            <h3 className="text-xl font-bold text-sky-500 mb-2">
+              <NumberFlow
+                value={1000000}
+                prefix="$"
+                suffix="M in Costs Saved"
+                transformTiming={{ duration: 150, easing: "linear(...)" }}
+                animated={true}
+              />
+            </h3>
             <p className="text-gray-600">
               {
                 "Our AI-driven solution to meet the unique needs of your business, ensuring you stay ahead of the competition."
